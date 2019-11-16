c8977a80651659e484616181297a038420b1d883
Henry Corrigan-Gibbs <henrycg@csail.mit.edu>
2019-11-16 10:50:01 +0100

    Stub puncturable set implementation
    This is the naive information-theoretic implementation of
    a puncturable set. Later on, we can swap this out for a
    computationally secure implementation as needed.

---
+++ pset.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+  "math/rand"
+)
+
+type Set map[int]bool
+
+type SetKey struct {
+  univSize int
+  delta int
+  set map[int]bool
+}
+
+type PuncSetKey struct {
+  univSize int
+  delta int
+  set map[int]bool
+}
+
+// Go's % operator follows C semantics and can produce
+// negative values if it's given a negative argument. 
+// We need an arithmetic mod operator.
+func MathMod(x int, mod int) int {
+  out := x % mod
+
+  // TODO: This is not a constant-time operation.
+  if out < 0 {
+    out = out + mod
+  }
+
+  return out
+}
+
+func (s Set) RandomValue(src *rand.Rand) int {
+  keys := make([]int, len(s))
+  i := 0
+  for k := range s {
+    keys[i] = k
+    i += 1
+  }
+
+  choose := src.Intn(len(keys))
+  return keys[choose]
+}
+
+func SetGen(src *rand.Rand, univSize int, setSize int) *SetKey {
+  if (univSize < setSize) {
+     panic("Set size too large.")
+  }
+
+  if (univSize < 1) {
+     panic("Universe size too small.")
+  }
+
+  // TODO: Implement this more efficiently
+  out := make(map[int]bool)
+  for len(out) < setSize {
+    out[src.Intn(univSize)] = true
+  }
+
+  delta := src.Intn(univSize)
+  return &SetKey{univSize, delta, out}
+}
+
+func SetGenWith(src *rand.Rand, univSize int, setSize int, val int) *SetKey {
+  key := SetGen(src, univSize, setSize)
+  key.delta = 0
+
+  // TODO: Implement this more efficiently.
+  set := key.Eval()
+  choose := set.RandomValue(src)
+  key.delta = MathMod(val - choose, univSize)
+
+  return key
+}
+
+func (key *SetKey) Shift(amount int) {
+  key.delta = MathMod(key.delta + amount, key.univSize)
+}
+
+func (key *SetKey) Punc(idx int) *PuncSetKey {
+  puncAt := MathMod(idx - key.delta, key.univSize)
+
+  if !key.set[puncAt] {
+    panic("Can't puncture at this point!")
+  }
+
+  out := make(map[int]bool)
+  for i := range key.set {
+    if i != puncAt {
+      out[i] = true
+    }
+  }
+
+  return &PuncSetKey{key.univSize, key.delta, out}
+}
+
+func (key *SetKey) Eval() Set {
+  return evalMap(key.univSize, key.delta, key.set)
+}
+
+func (key *PuncSetKey) Eval() Set {
+  return evalMap(key.univSize, key.delta, key.set)
+}
+
+func evalMap(univSize int, delta int, m map[int]bool) Set {
+  out := make(Set)
+
+  for k := range m {
+    out[MathMod(k + delta, univSize)] = true
+  }
+
+  return out
+}
+
---
+++ pset_test.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestMathMod(t *testing.T) {
+  assert.Assert(t, MathMod(5, 10) == 5)
+  assert.Assert(t, MathMod(10, 10) == 0)
+  assert.Assert(t, MathMod(-2, 10) == 8)
+  assert.Assert(t, MathMod(-1, 10) == 9)
+  assert.Assert(t, MathMod(-10, 10) == 0)
+  assert.Assert(t, MathMod(20, 10) == 0)
+  assert.Assert(t, MathMod(27, 10) == 7)
+  assert.Assert(t, MathMod(-27, 10) == 3)
+  assert.Assert(t, MathMod(-30, 10) == 0)
+  assert.Assert(t, MathMod(-100, 10) == 0)
+  assert.Assert(t, MathMod(-99, 10) == 1)
+}
+
+func randSource() *rand.Rand {
+  return rand.New(rand.NewSource(17))
+}
+
+func checkSet(t *testing.T, set Set, univSize int, setSize int) {
+  assert.Equal(t, len(set), setSize)
+
+  for v := range(set) {
+    assert.Assert(t, v < univSize && v >= 0)
+  }
+}
+
+func testPuncSetGenOnce(t *testing.T, univSize int, setSize int) {
+  key := SetGen(randSource(), univSize, setSize)
+  set := key.Eval()
+  checkSet(t, set, univSize, setSize)
+}
+
+func TestPuncSetGen_10_5(t *testing.T) {
+  testPuncSetGenOnce(t, 10, 5)
+}
+
+func TestPuncSetGen_10_10(t *testing.T) {
+  testPuncSetGenOnce(t, 10, 10)
+}
+
+func TestPuncSetGen_1_1(t *testing.T) {
+  testPuncSetGenOnce(t, 1, 1)
+}
+
+func TestPuncSetGen_100000_10(t *testing.T) {
+  testPuncSetGenOnce(t, 100000, 10)
+}
+
+func testPuncSetGenWith(t *testing.T, univSize int, setSize int, with int) {
+  key := SetGenWith(randSource(), univSize, setSize, with)
+  set := key.Eval()
+  checkSet(t, set, univSize, setSize)
+
+  inSet := false
+  for v := range(set) {
+    inSet = inSet || (with == v)
+  }
+
+  assert.Assert(t, inSet)
+}
+
+func TestPuncSetGenWith_10_5(t *testing.T) {
+  testPuncSetGenWith(t, 10, 5, 0)
+}
+
+func TestPuncSetGenWith_10_10(t *testing.T) {
+  testPuncSetGenWith(t, 10, 10, 8)
+}
+
+func TestPuncSetGenWith_1_1(t *testing.T) {
+  testPuncSetGenWith(t, 1, 1, 0)
+}
+
+func TestPuncSetGenWith_100000_10(t *testing.T) {
+  testPuncSetGenWith(t, 100000, 10, 7)
+}
+
+func testPuncSetGenWithPunc(t *testing.T, univSize int, setSize int, with int) {
+  key := SetGenWith(randSource(), univSize, setSize, with)
+  set := key.Eval()
+  checkSet(t, set, univSize, setSize)
+
+  inSet := false
+  for v := range(set) {
+    inSet = inSet || (with == v)
+  }
+  assert.Assert(t, inSet)
+
+  pkey := key.Punc(with)
+  pset := pkey.Eval()
+  assert.Equal(t, len(pset), setSize-1)
+
+  inSet = false
+  for v := range(pset) {
+    inSet = inSet || (with == v)
+  }
+
+  assert.Assert(t, !inSet)
+}
+
+
+func TestPuncSetGenWithPunc_10_5(t *testing.T) {
+  testPuncSetGenWithPunc(t, 10, 5, 0)
+}
+
+func TestPuncSetGenWithPunc_10_10(t *testing.T) {
+  testPuncSetGenWithPunc(t, 10, 10, 8)
+}
+
+func TestPuncSetGenWithPunc_1_1(t *testing.T) {
+  testPuncSetGenWithPunc(t, 1, 1, 0)
+}
+
+func TestPuncSetGenWithPunc_100000_10(t *testing.T) {
+  testPuncSetGenWithPunc(t, 100000, 10, 7)
+}
+
+
+func TestPuncSetShift(t *testing.T) {
+  key := SetGen(randSource(), 10, 5)
+  set := key.Eval()
+
+  key.Shift(1)
+  set2 := key.Eval()
+
+  for i := range set2 {
+    j := MathMod(i - 1, key.univSize)
+    assert.Assert(t, set[j])
+  }
+}
+
