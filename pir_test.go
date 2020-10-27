db11ad36568ee1aa733286954d6a007c44fdd8f2
Dima Kogan <dkogan@cs.stanford.edu>
2020-10-27 12:10:38 -0700

    comm benchmark

--- pir_test.go
+++ pir_test.go
@@ -30,6 +30,52 @@ func TestPIRPunc(t *testing.T) {
 	assert.DeepEqual(t, val, db[readIndex])
 }
 
+func TestPunc(t *testing.T) {
+	driver, err := ServerDriver()
+	assert.NilError(t, err)
+
+	assert.NilError(t, driver.Configure(TestConfig{
+		NumRows:    10000,
+		RowLen:     4,
+		PresetRows: []RowIndexVal{{7, 0x7, Row{'C', 'o', 'o', 'l'}}},
+		PirType:    Punc,
+		Updatable:  false,
+	}, nil))
+
+	//client, err := NewPirClientErasure(RandSource(), 1000, DEFAULT_CHUNK_SIZE, [2]PirServer{proxy, proxy})
+	client := NewPIRClient(NewPirClientPunc(RandSource()), RandSource(), [2]PirServer{driver, driver})
+
+	err = client.Init()
+	assert.NilError(t, err)
+
+	val, err := client.Read(0x7)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, val, Row("Cool"))
+}
+
+func TestMatrix(t *testing.T) {
+	driver, err := ServerDriver()
+	assert.NilError(t, err)
+
+	assert.NilError(t, driver.Configure(TestConfig{
+		NumRows:    10000,
+		RowLen:     4,
+		PresetRows: []RowIndexVal{{7, 0x7, Row{'C', 'o', 'o', 'l'}}},
+		PirType:    Matrix,
+		Updatable:  false,
+	}, nil))
+
+	//client, err := NewPirClientErasure(RandSource(), 1000, DEFAULT_CHUNK_SIZE, [2]PirServer{proxy, proxy})
+	client := NewPIRClient(NewPirClientMatrix(RandSource()), RandSource(), [2]PirServer{driver, driver})
+
+	err = client.Init()
+	assert.NilError(t, err)
+
+	val, err := client.Read(0x7)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, val, Row("Cool"))
+}
+
 // Not testing this for now since disabled it
 func DontTestPIRPuncKrzysztofTrick(t *testing.T) {
 	src := RandSource()
--- plotBenchmarkInitial.py
+++ plotBenchmarkInitial.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 import sys
 
-def plot(in_names, cols, pretty_col_names, out_name):
+def plot(in_names, cols, pretty_col_names, ylabel, out_name):
     fig, ax = plt.subplots()
 
     ax.set_xscale('log')
@@ -32,7 +32,7 @@ def plot(in_names, cols, pretty_col_names, out_name):
                 label=f'{pretty_col_names[idx+1]} ({pretty_in_name})')
 
         plt.xlabel(pretty_col_names[0])
-        plt.ylabel('Running time (ms)')
+        plt.ylabel(ylabel)
     fig.legend()
     plt.savefig(out_name)
 
@@ -49,13 +49,15 @@ args = parser.parse_args()
 
 
 names = args.input_files
-server_cols = [0, 1, 3]
-client_cols = [0, 2 ,4]
+server_cols = [0, 1, 4]
+client_cols = [0, 2 ,5]
+comm_cols = [0, 3, 6]
 
 pretty_col_names = ["Num Rows", "Offline", "Online"]
 
-plot(names, server_cols, pretty_col_names, args.out_basename+"_server.pdf")
-plot(names, client_cols, pretty_col_names, args.out_basename+"_client.pdf")
+plot(names, server_cols, pretty_col_names, 'Server Running time (ms)', args.out_basename+"_server.pdf")
+plot(names, client_cols, pretty_col_names, 'Client Running time (ms)', args.out_basename+"_client.pdf")
+plot(names, comm_cols, pretty_col_names, 'Bytes sent', args.out_basename+"_comm.pdf")
 
 
 
