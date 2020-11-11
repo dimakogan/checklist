7133d33171c96d5f8d0af73ab15e796d58057f16
Henry Corrigan-Gibbs <henrycg@csail.mit.edu>
2020-11-11 08:31:37 -0500

    Respond to Fetch API calls

--- proxy/main.go
+++ proxy/main.go
@@ -4,10 +4,10 @@
 package main
 
 import (
+	"crypto/sha256"
 	"crypto/tls"
 	"encoding/base64"
 	"errors"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,6 +16,7 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+  duration "github.com/golang/protobuf/ptypes/duration"
 	"github.com/google/safebrowsing"
 )
 
@@ -29,6 +30,9 @@ const (
 	mimeProto = "application/x-protobuf"
 
 	firefoxQueryRequestKey = "$req"
+
+  // in seconds
+  minimiumWaitDuration = 3
 )
 
 type proxyState struct {
@@ -36,26 +40,31 @@ type proxyState struct {
 	localIndex *LocalIndex
 }
 
-func handleUpdate(w http.ResponseWriter, req *http.Request) {
-	req.URL.Scheme = "https"
-	req.Host = safebrowsing.DefaultServerURL
-	req.URL.Host = req.Host
-	resp, err := http.DefaultTransport.RoundTrip(req)
-	if err != nil {
-		log.Printf("error: %v", err.Error())
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
+func unmarshalFetch(req *http.Request) (*FetchThreatListUpdatesRequest, error) {
+	q := req.URL.Query()
+	body, ok := q[firefoxQueryRequestKey]
+
+	if !ok {
+		return nil, errors.New("Query string does not contain $req field")
 	}
-	defer resp.Body.Close()
-	copyHeader(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-	log.Printf("header: %v", resp.Header)
-	io.Copy(w, resp.Body)
+
+	if len(body) != 1 {
+		return nil, errors.New("not sure what to do here")
+	}
+
+	data, err := base64.URLEncoding.DecodeString(body[0])
+	if err != nil {
+		return nil, err
+	}
+
+	fetchHash := &FetchThreatListUpdatesRequest{}
+	err = proto.Unmarshal(data, fetchHash)
+  return fetchHash, err
 }
 
 // From sbserver source
 // unmarshal reads pbResp from req. The mime will either be JSON or ProtoBuf.
-func unmarshal(req *http.Request) (*FindFullHashesRequest, error) {
+func unmarshalFind(req *http.Request) (*FindFullHashesRequest, error) {
 	q := req.URL.Query()
 	body, ok := q[firefoxQueryRequestKey]
 
@@ -104,9 +113,80 @@ func newThreatMatch(tt ThreatType, bytesIn []byte) *ThreatMatch {
 
 }
 
+/*
+func handleUpdate(w http.ResponseWriter, req *http.Request) {
+	req.URL.Scheme = "https"
+	req.Host = safebrowsing.DefaultServerURL
+	req.URL.Host = req.Host
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		log.Printf("error: %v", err.Error())
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	log.Printf("header: %v", resp.Header)
+	io.Copy(w, resp.Body)
+}*/
+
+func hashesToBigString(hashes []PartialHash) []byte {
+  out := make([]byte, len(hashes) * PartialHashLen)
+  for i,v := range hashes {
+    copy(out[i*PartialHashLen:(i+1)*PartialHashLen], v[:])
+  }
+  return out
+}
+
+func computeSum(hashes []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(hashes))
+  return hash.Sum(nil)
+}
+
+func handleFetch(state *proxyState, w http.ResponseWriter, req *http.Request) {
+  /*
+	upReq := new(FetchThreatListUpdatesRequest)
+	upReq, err := unmarshalFetch(req)
+	if err != nil {
+		log.Printf("Can't unmarshal: %v", err)
+		return
+	}
+
+  nLists := len(upReq.ListUpdateRequests)
+  */
+
+  hashes := hashesToBigString(getPartialHashes())
+
+	resp := new(FetchThreatListUpdatesResponse)
+	resp.ListUpdateResponses = make([]*FetchThreatListUpdatesResponse_ListUpdateResponse, 1)
+  resp.MinimumWaitDuration = new(duration.Duration)
+  resp.MinimumWaitDuration.Seconds = minimiumWaitDuration
+
+  listUp := new(FetchThreatListUpdatesResponse_ListUpdateResponse)
+  listUp.ThreatType = ThreatType_MALWARE
+  listUp.ThreatEntryType = ThreatEntryType_URL
+  listUp.PlatformType = PlatformType_ALL_PLATFORMS
+  listUp.ResponseType = FetchThreatListUpdatesResponse_ListUpdateResponse_FULL_UPDATE
+  listUp.Additions = make([]*ThreatEntrySet, 1)
+  listUp.Additions[0] = new(ThreatEntrySet)
+  listUp.Additions[0].RawHashes = new(RawHashes)
+  listUp.Additions[0].RawHashes.PrefixSize = PartialHashLen
+  listUp.Additions[0].RawHashes.RawHashes = hashes
+  listUp.NewClientState = []byte("c3RhdGUgaXMgbm8gc3RhdGU=")
+
+  listUp.Checksum = new(Checksum)
+  listUp.Checksum.Sha256 = computeSum(listUp.Additions[0].RawHashes.RawHashes)
+
+  resp.ListUpdateResponses[0] = listUp
+
+	marshal(w, resp)
+}
+
 func handleFind(state *proxyState, w http.ResponseWriter, req *http.Request) {
 	hashReq := new(FindFullHashesRequest)
-	hashReq, err := unmarshal(req)
+	hashReq, err := unmarshalFind(req)
 	if err != nil {
 		log.Printf("Can't unmarshal: %v", err)
 		return
@@ -134,14 +214,16 @@ func handleFind(state *proxyState, w http.ResponseWriter, req *http.Request) {
 	marshal(w, resp)
 }
 
+
+
 func handleHTTP(state *proxyState, w http.ResponseWriter, req *http.Request) {
 	log.Printf("%v", req.Host)
 	log.Printf("%v", req.URL)
 	log.Printf("%v", req.URL.Host)
 
 	if strings.HasPrefix(req.URL.Path, pathUpdate) {
-		log.Printf("UPDATE")
-		handleUpdate(w, req)
+		log.Printf("FETCH")
+		handleFetch(state, w, req)
 		return
 	} else if strings.HasPrefix(req.URL.Path, pathFind) {
 		log.Printf("FIND")
--- proxy/pir_query.go
+++ proxy/pir_query.go
@@ -4,13 +4,25 @@ import (
 	"crypto/sha256"
 )
 
-const evilHost = "testsafebrowsing.appspot.com/s/phishing.html"
+//const evilHost = "testsafebrowsing.appspot.com/s/phishing.html"
+const evilHost = "https://testsafebrowsing.appspot.com/apiv4/IOS/MALWARE/URL/"
+
+const PartialHashLen = 4
+type PartialHash = []byte
+
+func evilHash() []byte {
+	hash := sha256.New()
+	hash.Write([]byte(evilHost))
+  return hash.Sum(nil)
+}
+
+func getPartialHashes() []PartialHash {
+  return []PartialHash{ evilHash()[0:PartialHashLen] }
+}
 
 // This is the function that will make the PIR query.
 // It takes as input an index `idx` and outputs the
 // full SHA256 hash of the URL corresponding to that index.
 func queryForHash(idx int) []byte {
-	hash := sha256.New()
-	hash.Write([]byte(evilHost))
-	return hash.Sum(nil)
+	return evilHash()
 }
