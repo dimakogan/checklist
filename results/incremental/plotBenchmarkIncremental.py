29ef327d61a30da8ed3faa51e2c53094157ddf2f
Dima Kogan <dkogan@cs.stanford.edu>
2021-02-02 20:53:46 -0800

    edit

--- results/incremental/plotBenchmarkIncremental.py
+++ results/incremental/plotBenchmarkIncremental.py
@@ -10,6 +10,7 @@ matplotlib.use('Agg')
 import math
 import datetime
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import numpy as np
 import os
 import sys
@@ -67,38 +68,57 @@ boosted_data = np.genfromtxt(boosted, comments='#', skip_header=2)
 dpf_data = np.genfromtxt(dpf, comments='#', skip_header=2)
 matrix_data = np.genfromtxt(matrix, comments='#', skip_header=2)
 
-num_queries = np.linspace(1,20)
+num_queries = np.logspace(-7,7, base=2)
 
 fig, ax = plt.subplots()
 
+plt.ticklabel_format(style='plain')
+ax.set_xscale('log', base=2)
+ax.set_yscale('log')
+
+def form(x,p):
+    if x>=1: 
+        return ('%f' % x).rstrip('0').rstrip('.')
+    else:
+        return ('1/%f' % int(1/x)).rstrip('0').rstrip('.')
+
+f = FuncFormatter(matplotlib.ticker.FuncFormatter(form))
+ax.xaxis.set_major_formatter(f)
+ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x,p: ('%f' % x).rstrip('0').rstrip('.')))
+
 custom_style.remove_chart_junk(plt, ax, grid=True)
-ax.set_xscale('linear')
-ax.set_yscale('linear')
+
+
+ax.set_yticks([0.1,1,10,100])
+ax.set_xticks([1.0/100, 1.0/10,1,10,100])
+plt.ylim(bottom=0.2, top=400)
 
 
 linestyles = ["solid", "dashed", "dotted"]
 colors=["red", "blue", "green", "purple"]
 
+
+
 per_query_time, per_change_comm = compute_with_num_queries(boosted_data, num_queries)
-plt.plot(num_queries,per_query_time[0,:],
-         color=colors[0],
-         marker="o",
-         markevery=3,
-         linewidth=1,
-         label=('Checklist ($B=%d$)' % boosted_data[0,col_update_size]))
-plt.plot(num_queries,per_query_time[1,:],
-         color=colors[0],
-         marker="*",
-         markersize=5,
-         markevery=3,
-         linewidth=1,
-         label=('Checklist ($B=%d$)' % boosted_data[1,col_update_size]))
+# plt.plot(num_queries,per_query_time[0,:],
+#          color=colors[0],
+#          marker="o",
+#          markevery=3,
+#          linewidth=1,
+#          label=('Checklist ($B=%d$)' % boosted_data[0,col_update_size]))
+# plt.plot(num_queries,per_query_time[1,:],
+#          color=colors[0],
+#          marker="*",
+#          markersize=5,
+#          markevery=3,
+#          linewidth=1,
+#          label=('Checklist ($B=%d$)' % boosted_data[1,col_update_size]))
 plt.plot(num_queries,per_query_time[2,:],
          color=colors[0],
          markevery=3,
          marker="d",
          linewidth=1,
-         label=('Checklist ($B=%d$)' % boosted_data[2,col_update_size]))
+         label=('Checklist'))
 
 
 per_query_time, per_change_comm = compute_with_num_queries(dpf_data, num_queries)
@@ -114,10 +134,11 @@ plt.plot(num_queries,per_query_time[1,:],
          label='Matrix')
 
 
-plt.xlabel("Number of queries per period")
+plt.xlabel("Number of queries per update")
 plt.ylabel("Amortized server time\nper query [ms]")
-plt.xlim(xmin=0.0)
-plt.ylim(ymin=0.0)
+#ax.set_yticks([0.2, 1, 2, 4, 5])
+plt.minorticks_off()
+
 
 fig.legend(fontsize=6)
 custom_style.save_fig(fig, "server.pdf", [2.3, 1.8])
